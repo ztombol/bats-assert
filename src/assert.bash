94988734ca1d4debbc72ec6c13337860846710bd
Jason Karns <jason@karns.name>
2016-02-19 12:00:18 -0500

    Add refute assertion

--- src/assert.bash
+++ src/assert.bash
@@ -71,6 +71,28 @@ assert() {
   fi
 }
 
+# Fail and display the expression if it evaluates to true.
+#
+# NOTE: The expression must be a simple command. Compound commands, such
+#       as `[[', can be used only when executed with `bash -c'.
+#
+# Globals:
+#   none
+# Arguments:
+#   $1 - expression
+# Returns:
+#   0 - expression evaluates to FALSE
+#   1 - otherwise
+# Outputs:
+#   STDERR - details, on failure
+refute() {
+  if "$@"; then
+    batslib_print_kv_single 10 'expression' "$*" \
+      | batslib_decorate 'assertion succeeded, but it was expected to fail' \
+      | fail
+  fi
+}
+
 # Fail and display details if the expected and actual values do not
 # equal. Details include both values.
 #
