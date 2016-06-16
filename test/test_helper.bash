2a70800458d49a9e6d82c416aed93ae801d95308
Jason Karns <jason@karns.name>
2016-06-16 10:17:41 -0400

    load the bundled bats-support

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,9 +1,7 @@
 setup() {
-  export TEST_MAIN_DIR="${BATS_TEST_DIRNAME}/.."
-  export TEST_DEPS_DIR="${TEST_DEPS_DIR-${TEST_MAIN_DIR}/..}"
 
   # Load dependencies.
-  load "${TEST_DEPS_DIR}/bats-support/load.bash"
+  load "helpers/bats-support/load"
 
   # Load library.
   load '../load'
